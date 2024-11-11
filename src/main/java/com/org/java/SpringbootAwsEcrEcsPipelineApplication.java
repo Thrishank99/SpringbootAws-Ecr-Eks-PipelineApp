b50a38e3d9f0c76e6e20e76bd4ae1851ed14227e
Thrishank99 <thrishank966@gmail.com>
2024-11-11 15:49:47 +0530

    thfgdcxz

--- src/main/java/com/org/java/SpringbootAwsEcrEcsPipelineApplication.java
+++ src/main/java/com/org/java/SpringbootAwsEcrEcsPipelineApplication.java
@@ -8,7 +8,7 @@ public class SpringbootAwsEcrEcsPipelineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAwsEcrEcsPipelineApplication.class, args);
-		System.out.println("hello");
+		
 	}
 
 }
